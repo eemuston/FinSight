530126623c2720ed6f9f95f7f6f7b381bfe18347
eemuston <eemeli.mustonen15@gmail.com>
2026-03-18 13:31:41 +0200

    Created simple pdf uploading endpoint using multer.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,15 @@
 import express from 'express';
+import multer from 'multer';
 const app = express();
+const upload = multer({ dest: 'uploads/' });
 app.use(express.json());
 
 const PORT = 3000;
 
-app.get('/ping', (_req, res) => {
-    console.log('täälähän pelataan pinki ponkia');
-    res.send('pong');
-});
+app.post('/pdf', upload.single('pdf'), (req, res) => {
+    console.log(req.file)
+    res.send(req.file)
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
