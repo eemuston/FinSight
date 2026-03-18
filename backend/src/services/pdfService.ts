64ae8d18789b8863bb449e43be0bdb1fae08d6ee
eemuston <eemeli.mustonen15@gmail.com>
2026-03-18 17:33:44 +0200

    PDF to vectorstorage pipline working. Extracting, chunking, embedding, and pushing to Qdrant vectorstorage

--- backend/src/services/pdfService.ts
+++ backend/src/services/pdfService.ts
@@ -1,5 +1,6 @@
 import Anthropic from '@anthropic-ai/sdk'
 import { VoyageAIClient } from 'voyageai'
+import { QdrantClient } from '@qdrant/js-client-rest'
 import type { Express } from 'express'
 import fs from 'fs'
 
@@ -9,6 +10,10 @@ const anthropic = new Anthropic({
 
 const vo = new VoyageAIClient({
     apiKey: process.env.VOYAGE_API_KEY
+})
+
+const qdrant = new QdrantClient({
+    url: process.env.QDRANT_URL
 })
 
 const extractPdfWithClaude = async (file: Express.Multer.File) => {
@@ -52,19 +57,43 @@ const chunkText = (text: string, chunkSize: number = 500, overlap: number = 50):
     return chunks
 }
 
-const embeddings = async (chunks: string[]) => {
+const embeddings = async (chunks: string[]): Promise<number[][]> => {
     const result = await vo.embed({
         input: chunks,
-        model: "voyage-3"
+        model: "voyage-finance-2"
     })
-    return result.data?.map(item => item.embedding)
+    return (result.data ?? []).map(item => item.embedding ?? [])
 }
 
+const collectionCreation = async () => {
+    const collections = await qdrant.getCollections()
+    const exists = collections.collections.some(c => c.name === 'test')
+    if (! exists){
+        await qdrant.createCollection('test', {
+            vectors: {
+                size: 1024,
+                distance: 'Cosine'
+            }
+        })
+    }
+}        
+
+const storeInQdrant = async (chunks: string [], embeddings: number[][]) => {
+    await qdrant.upsert('test', {
+        points: chunks.map((chunk, index) => ({
+            id: index,
+            vector: embeddings[index],
+            payload: { text: chunk }
+        }))
+    })
+}
 
 const processPdf = async (file: Express.Multer.File) => {
     const extractedText = await extractPdfWithClaude(file)
     const chunkedText = chunkText(extractedText)
     const embeddedText = await embeddings(chunkedText)
+    await collectionCreation()
+    await storeInQdrant(chunkedText, embeddedText)
     console.log(extractedText)
     console.log(embeddedText)
     return { message: 'received'}
