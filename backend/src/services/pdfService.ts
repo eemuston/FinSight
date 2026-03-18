2bc5126f41568ca6929d612dbd6ee3d47fc096cb
eemuston <eemeli.mustonen15@gmail.com>
2026-03-18 15:43:47 +0200

    Integrated Anthropic API for PDF parsing with AI, also changed the model to cheaper option to save tokens.

--- backend/src/services/pdfService.ts
+++ backend/src/services/pdfService.ts
@@ -1,6 +1,41 @@
+import Anthropic from '@anthropic-ai/sdk'
+import fs from 'fs'
+
+const anthropic = new Anthropic({
+    apiKey: process.env.ANTHROPIC_API_KEY
+})
+
+const extractPdfWithClaude = async (file: any) => {
+    const pdfBuffer = fs.readFileSync(file.path)
+    const base64Pdf = pdfBuffer.toString('base64')
+
+    const response = await anthropic.messages.create({
+        model: "claude-haiku-4-5-20251001",
+        max_tokens: 300,
+        messages: [{
+            role: 'user',
+            content: [{
+                type: 'document',
+                source: {
+                    type: 'base64',
+                    media_type: 'application/pdf',
+                    data: base64Pdf
+                }
+            },
+            {
+                type: 'text',
+                text: 'Extract all text from this document.'
+            }
+        ]
+        }]
+    })
+    return response;
+}
+
+
 const processPdf = async (file: any) => {
-    console.log('pööö')
-    console.log(file)
+    const content = await extractPdfWithClaude(file)
+    console.log(content)
     return { message: 'received'}
 }
 
